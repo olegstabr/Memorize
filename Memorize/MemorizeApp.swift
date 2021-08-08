65e4eff1fb45f8f0e39e53f83cf23b56d839e549
oleg stabrovsky <olegstabr@gmail.com>
2021-08-08 17:56:21 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Олег Стабровский on 08.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
